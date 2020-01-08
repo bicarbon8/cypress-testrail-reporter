0f0e19dc9c5f59cb6d811eb0244cabcaf49477d8
Jason Holt Smith <jason.smith@verizonconnect.com>
2020-01-08 18:16:39 +0000

    removing additional checks on results posting as it was unreliable

--- src/testrail-api.js
+++ src/testrail-api.js
@@ -64,31 +64,20 @@ class TestRailApi {
   };
 
   async publishResults(results) {
-    var addedResults = [];
     if (this.options.usePlan === true) {
       for(var i=0; i<results.length; i++) {
         try {
           var test = await this.getTestByCaseId(results[i].case_id);
-          let trs = await this._post('add_result', test.id.toString(), results[i]);
-          if (trs && trs.length > 0) {
-            addedResults.push(...trs);
-          } else {
-            throw new Error(`unable to add result of '${JSON.stringify(results[i])}' for test '${JSON.stringify(test.id)}'`);
-          }
+          await this._post('add_result', test.id.toString(), results[i]);
         } catch (e) {
           console.error(e);
         }
       }
     } else {
       try {
-        let trs = await this._post('add_results_for_cases', this.runId.toString(), { 
+        await this._post('add_results_for_cases', this.runId.toString(), { 
           results: results
         });
-        if (trs && trs.length > 0) {
-          addedResults.push(...trs);
-        } else {
-          throw new Error(`unable to add result of '${JSON.stringify(results[i])}' for test '${JSON.stringify(test.id)}'`);
-        }
       } catch(e) {
         console.error(e);
       };
